e0f1f328bde8177fd6843ab93ca6b974dcc62f31
Mario Suharja <mariostefanos@gmail.com>
2016-11-22 23:04:39 +0700

    Home Route with CSS Module

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { Navigation } from 'components'
+import { container, innerContainer } from './styles.css'
 
 class MainContainer extends Component {
   render () {
     return (
-      <p>{'Hello World!'}</p>
+      <div className={container}>
+        <Navigation isAuthed={false} />
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
+      </div>
     )
   }
+}
+
+MainContainer.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default MainContainer
