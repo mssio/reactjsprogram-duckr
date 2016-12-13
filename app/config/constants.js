255cdd5b47a5a2ffa7a9633597a66f6d40fb75f5
Mario Suharja <mariostefanos@gmail.com>
2016-12-14 00:26:04 +0700

    Authentication with Firebase

---
+++ app/config/constants.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBycwJW1gay3nftQgRKwSIdAipD5SrOQbQ',
+  authDomain: 'duckr-ceeea.firebaseapp.com',
+  databaseURL: 'https://duckr-ceeea.firebaseio.com',
+  storageBucket: 'duckr-ceeea.appspot.com',
+  messagingSenderId: '280509723324',
+}
+
+firebase.initializeApp(config)
+
+export const ref = firebase.database().ref()
+export const firebaseAuth = firebase.auth
--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,13 +1,8 @@
+import firebase from 'firebase'
+import { ref, firebaseAuth } from 'config/constants'
+
 export default function auth () {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        name: 'Mario Suharja',
-        avatar: 'https://pbs.twimg.com/profile_images/698480433/57155393.jpg',
-        uid: 'mssio',
-      })
-    }, 2000)
-  })
+  return firebaseAuth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
 }
 
 export function checkIfAuthed (store) {
@@ -16,5 +11,11 @@ export function checkIfAuthed (store) {
 }
 
 export function logout () {
-  console.log('logout')
+  return firebaseAuth().signOut()
+}
+
+export function saveUser (user) {
+  return ref.child(`users/${user.uid}`)
+    .set(user)
+    .then(() => user)
 }
